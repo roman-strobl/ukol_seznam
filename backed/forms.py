ca4b76500980b66704de56accf0560dc1b011a2f
Roman Strobl <strobl17@seznam.cz>
2022-11-07 13:03:05 +0100

    Možnost filtrovat filmy podle Features

---
+++ backed/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+
+from .models import Features
+
+class FeatureForm(forms.Form):
+    
+    Features_db = Features.objects.all()
+
+    OPTIONS = list()
+
+    for feature in Features_db:
+        
+        _, name = feature.name.split("_",1)
+        OPTIONS.append((feature.id,name))
+
+    print(OPTIONS)
+
+    Features = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple, choices=OPTIONS,required=False)
--- backed/views.py
+++ backed/views.py
@@ -1,9 +1,34 @@
 from django.shortcuts import render
+from django.db.models import Q
 
 # Create your views here.
 from django.http import HttpResponse
 from .models import Movie, Features
+from .forms import FeatureForm
 
 def index(request):
-    movies = Movie.objects.all()
-    return render(request, 'list/main_view.html', {"movies": movies})
+    movies = None
+    if request.method == 'POST':
+        form = FeatureForm(request.POST)
+        if form.is_valid():
+            filters = Q()
+            features = form.cleaned_data.get('Features')
+            print(f"Selected: {features}")
+            for feature in features:
+                filters &= Q(features__in=feature)
+            
+            if features == []:
+                movies = Movie.objects.all()
+            else:           
+                movies = Movie.objects.filter(filters)
+            
+
+    else:
+        form = FeatureForm
+        movies = Movie.objects.all()
+
+    #return render_to_response('render_country.html', {'form': form},
+    #                          context_instance=RequestContext(request))
+
+    
+    return render(request, 'list/main_view.html', {'form': form, "movies": movies})
